cba7deac0ac90edc2e34042b9e06ca8008f25c55
Aakash447 <s.aakash.lh@gmail.com>
2020-12-19 23:49:03 +0530

    added http-proxy-middleware

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://13.235.90.125:2112',
+      changeOrigin: true,
+    })
+  );
+};

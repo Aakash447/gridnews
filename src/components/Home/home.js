a45cb2e59ce62babd18194b652bdb7c5c3c8589c
Aakash447 <s.aakash.lh@gmail.com>
2020-12-20 17:48:28 +0530

    added jsonplaceholder

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -4,8 +4,16 @@ import Showcase from './Showcase'
 import Polls from './Polls'
 import './css/home.scss'
 import './css/utility.scss'
+import axios from 'axios'
 
 function Home() {
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res=>{
+            console.log('response:',res.data)
+        }).catch(err=>{
+            console.log('err:',err)
+        })
+    })
     return (
         <>
             <Header/>
